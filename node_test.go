ba1582f873591ba36a0faabf28777d39d8917470
gernest <geofreyernest@live.com>
2024-04-26 23:20:16 +0300

    add tests for writing otel data

--- node_test.go
+++ node_test.go
@@ -273,6 +273,86 @@ func TestSchema_LogssData(t *testing.T) {
 	match(t, "testdata/otel_logs_data_schema.txt", schema.String())
 }
 
+func TestAppendMessage_metricsData(t *testing.T) {
+	msg := &metricsv1.MetricsData{}
+	b := build(msg.ProtoReflect())
+	b.build(memory.DefaultAllocator)
+	b.append(msg.ProtoReflect())
+	msg.ResourceMetrics = []*metricsv1.ResourceMetrics{
+		{ScopeMetrics: []*metricsv1.ScopeMetrics{
+			{Metrics: []*metricsv1.Metric{
+				{Name: "check", Data: &metricsv1.Metric_Gauge{
+					Gauge: &metricsv1.Gauge{
+						DataPoints: []*metricsv1.NumberDataPoint{
+							{TimeUnixNano: 16, Value: &metricsv1.NumberDataPoint_AsInt{
+								AsInt: 18,
+							}},
+						},
+					},
+				}},
+			}},
+		}},
+	}
+	b.append(msg.ProtoReflect())
+
+	r := b.NewRecord()
+	data, err := r.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	match(t, "testdata/otel_metrics_data.json", string(data))
+}
+func TestAppendMessage_traceData(t *testing.T) {
+	msg := &tracev1.TracesData{}
+	b := build(msg.ProtoReflect())
+	b.build(memory.DefaultAllocator)
+	b.append(msg.ProtoReflect())
+	msg.ResourceSpans = []*tracev1.ResourceSpans{
+		{ScopeSpans: []*tracev1.ScopeSpans{
+			{Spans: []*tracev1.Span{
+				{TraceId: []byte("hello")},
+			}},
+		}},
+	}
+	b.append(msg.ProtoReflect())
+
+	r := b.NewRecord()
+	data, err := r.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	match(t, "testdata/otel_trace_data.json", string(data))
+}
+
+func TestAppendMessage_LogsData(t *testing.T) {
+	msg := &logsv1.LogsData{}
+	b := build(msg.ProtoReflect())
+	b.build(memory.DefaultAllocator)
+	b.append(msg.ProtoReflect())
+	msg.ResourceLogs = []*logsv1.ResourceLogs{
+		{ScopeLogs: []*logsv1.ScopeLogs{
+			{LogRecords: []*logsv1.LogRecord{
+				{
+					Body: &commonv1.AnyValue{
+						Value: &commonv1.AnyValue_StringValue{
+							StringValue: "hello",
+						},
+					},
+					SeverityText: "DEBUG",
+				},
+			}},
+		}},
+	}
+	b.append(msg.ProtoReflect())
+
+	r := b.NewRecord()
+	data, err := r.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	match(t, "testdata/otel_logs_data.json", string(data))
+}
+
 func match(t testing.TB, path string, value string, write ...struct{}) {
 	t.Helper()
 	if len(write) > 0 {
