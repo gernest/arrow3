c9152b3ca00649888505beed75b5d98fc32b8e94
gernest <geofreyernest@live.com>
2024-04-28 17:43:16 +0300

    fix reading one of fields

--- node_test.go
+++ node_test.go
@@ -37,12 +37,6 @@ func TestMessage_scalarRepeated(t *testing.T) {
 	schema := msg.schema.String()
 	match(t, "testdata/scalar_repeated.txt", schema)
 }
-func TestMessage_scalarMap(t *testing.T) {
-	m := &samples.ScalarTypesMap{}
-	msg := build(m.ProtoReflect())
-	schema := msg.schema.String()
-	match(t, "testdata/scalar_map.txt", schema)
-}
 func TestMessage_Nested00(t *testing.T) {
 	m := &samples.Nested{}
 	msg := build(m.ProtoReflect())
@@ -149,22 +143,7 @@ func TestAppendMessage_scalar_repeated(t *testing.T) {
 	}
 	match(t, "testdata/scalar_repeated.json", string(data))
 }
-func TestAppendMessage_scalar_map(t *testing.T) {
-	msg := &samples.ScalarTypesMap{}
-	b := build(msg.ProtoReflect())
-	b.build(memory.DefaultAllocator)
-	b.append(msg.ProtoReflect())
-	msg.Labels = map[string]string{
-		"key": "value",
-	}
-	b.append(msg.ProtoReflect())
-	r := b.NewRecord()
-	data, err := r.MarshalJSON()
-	if err != nil {
-		t.Fatal(err)
-	}
-	match(t, "testdata/scalar_map.json", string(data))
-}
+
 func TestAppendMessage_nested(t *testing.T) {
 	msg := &samples.Nested{
 		NestedRepeatedScalar: []*samples.ScalarTypes{{}},
